b09de69d907b0f40cbe711f710c4973d9452fb40
jinzhimin <jinzhiminCN@163.com>
2018-08-20 19:24:05 +0800

    add linear_regression_tf.py

--- tensorflow_basic/linear_regression_tf.py
+++ tensorflow_basic/linear_regression_tf.py
@@ -1,52 +1,50 @@
 # -*- coding:utf-8 -*-
 
 # ==============================================================================
-# tensorflow简单网络结构。
+# tensorflow版本的线性回归模型。
 # ==============================================================================
 import tensorflow as tf
 import os
+import math
+import numpy as np
+import matplotlib.pyplot as plt
 import config.common_config as com_config
-from tensorflow.examples.tutorials.mnist import input_data
 from util.log_util import LoggerUtil
+
 
 # 日志器
 common_logger = LoggerUtil.get_common_logger()
-# mnist数据
-mnist = input_data.read_data_sets(com_config.MNIST_DIR, one_hot=True)
-n_input = 784 # MNIST data input (img shape: 28*28)
-n_classes = 10 # MNIST total classes (0-9 digits)
 
 
-class BasicDNN(object):
+class LinearRegressionTF(object):
     """
-    最简单的一层神经网络结构。
+    线性回归模型TensorFlow版本。
     """
-    def __init__(self, input_dim, output_dim):
+    def __init__(self, x_data, y_data, input_dim):
         """
-        初始化网络数据
-        :param input_dim:
-        :param output_dim:
+        初始化线性函数。
+        :param x_data: 输入x数据
+        :param y_data: 输入y数据
         """
         # 网络结构参数
         self.x_input = None
-        self.y_label = None
+        self.y_input = None
+        self.input_dim = input_dim
         self.weight = None
         self.bias = None
-        self.input_dim = input_dim
-        self.output_dim = output_dim
+        self.x_data = x_data
+        self.y_data = y_data
 
         # 训练需要的超参数
         self.learning_rate = 0.1
-        self.training_epochs = 10
+        self.training_epochs = 2000
         self.batch_size = 100
         self.display_steps = 1
         self.save_steps = 1
 
         # 预测优化函数
-        self.y_predict = None
         self.y_value = None
         self.loss = None
-        self.accuracy = None
         self.optimizer = None
 
         # sess会话
@@ -57,8 +55,8 @@ class BasicDNN(object):
         self.merged_summary_op = None
 
         # 目录初始化
-        self.tf_logs_path = os.path.join(com_config.TF_MODEL_LOGS_DIR, "basicDNN")
-        self.checkpoints_path = os.path.join(com_config.TF_MODEL_CHECKPOINT_DIR, "basicDNN")
+        self.tf_logs_path = os.path.join(com_config.TF_MODEL_LOGS_DIR, "LinearRegressionTF")
+        self.checkpoints_path = os.path.join(com_config.TF_MODEL_CHECKPOINT_DIR, "LinearRegressionTF")
 
         # 变量和参数初始化
         self.init_network()
@@ -66,10 +64,10 @@ class BasicDNN(object):
     @staticmethod
     def name(self):
         """
-        网络结构名称。
+        结构名称。
         :return:
         """
-        return "Basic Deep Neural Network(DNN)"
+        return "Linear Regression TensorFlow(LR)"
 
     def init_network(self):
         """
@@ -100,9 +98,9 @@ class BasicDNN(object):
         :return:
         """
         # 模型的输入x值
-        self.x_input = tf.placeholder(tf.float32, [None, self.input_dim], name="x_input")
+        self.x_input = tf.placeholder(tf.float32, shape=[None, self.input_dim], name="x_input")
         # 模型的输入y值
-        self.y_label = tf.placeholder(tf.float32, [None, self.output_dim], name="y_label")
+        self.y_input = tf.placeholder(tf.float32, name="y_input")
 
     def variables(self):
         """
@@ -110,20 +108,24 @@ class BasicDNN(object):
         :return:
         """
         # 权重变量
-        self.weight = tf.Variable(tf.zeros([self.input_dim, self.output_dim]), name='Weights')
+        # self.weight = tf.Variable(tf.zeros([self.input_dim, 1]), name='Weights')
+        self.weight = tf.Variable(tf.random_uniform([self.input_dim, 1], -1.0, 1.0), name='Weights')
         # 偏置变量
-        self.bias = tf.Variable(tf.zeros([self.output_dim]), name='Bias')
+        # 偏置初始化为0
+        self.bias = tf.Variable(tf.zeros([1]), name='Bias')
+        # 偏置初始化为随机值
+        # self.bias = tf.Variable(np.random.randn(), name='Bias')
 
     def super_parameters(self):
         """
         设置超参数。
         :return:
         """
-        self.learning_rate = 1e-5
-        self.training_epochs = 10
-        self.batch_size = 100
-        self.display_steps = 1
-        self.save_steps = 1
+        self.learning_rate = 1e-3
+        self.training_epochs = 2000
+        self.batch_size = 1
+        self.display_steps = 50
+        self.save_steps = 100
 
     def init_variables(self):
         """
@@ -139,28 +141,24 @@ class BasicDNN(object):
         """
         # 计算预测值
         self.y_value = tf.matmul(self.x_input, self.weight) + self.bias
-        self.y_predict = tf.nn.softmax(self.y_value)
 
     def loss_function(self):
         """
         损失函数设置。
         :return:
         """
-        # 1. 直接计算交叉熵
-        self.loss = tf.reduce_mean(tf.reduce_sum(-self.y_label * tf.log(self.y_predict)))
-        # 2. 差平方求和再平均
-        # self.loss = tf.reduce_mean(tf.reduce_sum(tf.square(self.y_label - self.y_predict)))
-        # 3. 计算softmax交叉熵
-        # softmax_cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(labels=self.y_label, logits=self.y_value)
-        # self.loss = tf.reduce_mean(tf.reduce_sum(softmax_cross_entropy))
+        n_samples = len(self.x_data)
+        # self.loss = tf.reduce_sum(tf.pow(self.y_input - self.y_value, 2)) / (2 * n_samples)
+        # 1. 差平方求和
+        # self.loss = tf.reduce_mean(tf.square(self.y_input - self.y_value)) / (2 * n_samples)
+        self.loss = tf.reduce_mean(tf.square(self.y_input - self.y_value))
 
     def evaluate_function(self):
         """
         评价函数设置。
         :return:
         """
-        self.accuracy = tf.equal(tf.argmax(self.y_predict, 1), tf.argmax(self.y_label, 1))
-        self.accuracy = tf.reduce_mean(tf.cast(self.accuracy, tf.float32))
+        pass
 
     def solver(self):
         """
@@ -168,9 +166,9 @@ class BasicDNN(object):
         :return:
         """
         # 1. Gradient Descent
-        # self.optimizer = tf.train.GradientDescentOptimizer(self.learning_rate).minimize(self.loss)
+        self.optimizer = tf.train.GradientDescentOptimizer(self.learning_rate).minimize(self.loss)
         # 2. Adam
-        self.optimizer = tf.train.AdamOptimizer(self.learning_rate).minimize(self.loss)
+        # self.optimizer = tf.train.AdamOptimizer(self.learning_rate).minimize(self.loss)
 
     def summary(self):
         """
@@ -187,17 +185,6 @@ class BasicDNN(object):
         """
         with tf.Session() as sess:
             self.sess = sess
-            # 初始化
-            sess.run(self.initializer)
-            # 训练数据
-
-    def train_mnist(self):
-        """
-        训练mnist数据
-        :return:
-        """
-        with tf.Session() as sess:
-            self.sess = sess
             # 执行初始化
             sess.run(self.initializer)
             # summary writer
@@ -209,23 +196,28 @@ class BasicDNN(object):
             # 训练数据
             for epoch in range(self.training_epochs):
                 avg_cost = 0.
-                total_batch = int(mnist.train.num_examples / self.batch_size)
+                total_size = len(self.x_data)
+                total_batch = math.ceil(total_size / self.batch_size)
+
                 # 循环运行所有批次数据
                 for i in range(total_batch):
-                    batch_xs, batch_ys = mnist.train.next_batch(self.batch_size)
+                    limit_begin = i * self.batch_size
+                    limit_end = max(min((i + 1) * self.batch_size, total_size), 0)
+                    batch_xs = self.x_data[limit_begin:limit_end]
+                    batch_ys = self.y_data[limit_begin:limit_end]
 
-                    # 执行优化、损失函数、准确率
-                    _, cost_value, accuracy_value, summary = \
-                        sess.run([self.optimizer, self.loss, self.accuracy, self.merged_summary_op],
-                                 feed_dict={self.x_input: batch_xs, self.y_label: batch_ys})
+                    # 执行优化、损失函数
+                    _, cost_value, summary = \
+                        sess.run([self.optimizer, self.loss, self.merged_summary_op],
+                                 feed_dict={self.x_input: batch_xs, self.y_input: batch_ys})
 
                     # 在summary_writer中记录相应的训练过程
                     summary_writer.add_summary(summary, epoch * total_batch + i)
                     # 计算平均损失
                     avg_cost += cost_value / total_batch
 
-                    common_logger.info("Epoch: {0:0>4}_{1:0>4} cost={2:.9f} accuracy={3:.9f}".format(
-                        (epoch + 1), i, cost_value, accuracy_value))
+                    # common_logger.info("Epoch: {0:0>4}_{1:0>4} cost={2:.9f}".format(
+                    #     (epoch + 1), i, cost_value))
 
                 # 记录每轮迭代的中间结果
                 if (epoch + 1) % self.display_steps == 0:
@@ -233,15 +225,9 @@ class BasicDNN(object):
                 if (epoch + 1) % self.save_steps == 0:
                     saver.save(sess, self.checkpoints_path, global_step=(epoch + 1))
 
-            # 记录训练的最终结果
+            self.show_variable()
             saver.save(sess, self.checkpoints_path, global_step=(self.training_epochs + 1))
             common_logger.info("Optimization Finished!")
-
-            # 测试模型，计算测试数据的准确率
-            test_accuracy = self.accuracy.eval(
-                {self.x_input: mnist.test.images, self.y_label: mnist.test.labels})
-
-            common_logger.info("Accuracy:{0}".format(test_accuracy))
 
     def test(self):
         """
@@ -250,8 +236,48 @@ class BasicDNN(object):
         """
         pass
 
+    def show_variable(self):
+        """
+        输出变量的结果。
+        :return:
+        """
+        variable_names = [v.name for v in tf.trainable_variables()]
+        values = self.sess.run(variable_names)
+        for k, v in zip(variable_names, values):
+            common_logger.info("Variable: {0} {1}, {2}".format(k, v, v.shape))
+
+
+def plt_linear_model():
+    """
+    绘画线性模型的内容。
+    :return:
+    """
+    train_X = np.asarray([3.3, 4.4, 5.5, 6.71, 6.93, 4.168, 9.779, 6.182, 7.59,
+                          2.167, 7.042, 10.791, 5.313, 7.997, 5.654, 9.27, 3.1])
+    train_Y = np.asarray([1.7, 2.76, 2.09, 3.19, 1.694, 1.573, 3.366, 2.596, 2.53,
+                          1.221, 2.827, 3.465, 1.65, 2.904, 2.42, 2.94, 1.3])
+
+    weight1, bias1 = 0.08040322, 1.7962127
+    weight2, bias2 = 0.24164452, 0.8586517
+
+    x = np.linspace(0, 10, 10)
+    y1 = x * weight1 + bias1
+    y2 = x * weight2 + bias2
+
+    plt.figure()
+    plt.scatter(train_X, train_Y)
+    plt.plot(x, y1, 'r')
+    plt.plot(x, y2, 'b')
+    plt.show()
+
+
 if __name__ == "__main__":
-    basicDNN = BasicDNN(n_input, n_classes)
-    basicDNN.train_mnist()
-
-
+    # Training Data
+    train_X = np.asarray([3.3, 4.4, 5.5, 6.71, 6.93, 4.168, 9.779, 6.182, 7.59,
+                          2.167, 7.042, 10.791, 5.313, 7.997, 5.654, 9.27, 3.1])
+    train_Y = np.asarray([1.7, 2.76, 2.09, 3.19, 1.694, 1.573, 3.366, 2.596, 2.53,
+                          1.221, 2.827, 3.465, 1.65, 2.904, 2.42, 2.94, 1.3])
+    train_X = train_X.reshape((-1, 1))
+    linearReg = LinearRegressionTF(train_X, train_Y, train_X.shape[1])
+    linearReg.train()
+    # plt_linear_model()
