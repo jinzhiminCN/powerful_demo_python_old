74125675030aac80babc3942e35ac13b6ed7e689
jinzhimin <jinzhiminCN@163.com>
2018-08-21 15:47:46 +0800

    add TimedRotatingFileHandler in logging_demo.py

--- python_basic/logging_demo.py
+++ python_basic/logging_demo.py
@@ -5,9 +5,12 @@
 # ==============================================================================
 
 import os
-import datetime
+import sys
+import re
 import logging
 import logging.config
+from logging.handlers import TimedRotatingFileHandler
+from logging.handlers import RotatingFileHandler
 import yaml
 import config.common_config as com_config
 from config.common_config import COMMON_LOG_DIR
@@ -21,6 +24,7 @@ if not os.path.exists(COMMON_LOG_DIR):
     os.makedirs(COMMON_LOG_DIR)
 
 LOG_FILE = "application.log"
+DATE_LOG = "date_log"
 
 LOGGING_DICT = {
         "version": 1,
@@ -52,6 +56,16 @@ LOGGING_DICT = {
                 "backupCount": 20,
                 "encoding": "utf8"
             },
+
+            "date": {
+                "class": "logging.handlers.TimedRotatingFileHandler",
+                "level": "INFO",
+                "formatter": "simple",
+                "filename": os.path.join(COMMON_LOG_DIR, DATE_LOG),
+                'when': "D",
+                "backupCount": 20,
+                "encoding": "utf8"
+            },
         },
 
         "loggers": {
@@ -63,14 +77,14 @@ LOGGING_DICT = {
         },
 
         "root": {
-            'handlers': ['default'],
+            'handlers': ['default', 'date'],
             'level': "INFO",
             'propagate': False
         }
     }
 
 
-def test_dictConfig():
+def test_dict_config():
     """
     通过字典配置日志。
     :return:
@@ -101,30 +115,72 @@ def test_yaml_config():
     logger.critical('critical message')
 
 
-def test_basicConfig():
+def test_basic_config():
     """
     配置日志基本信息。
     :return:
     """
+    # 文件日志
     file_name = os.path.join(COMMON_LOG_DIR, LOG_FILE)
     logging.basicConfig(level=logging.DEBUG,
                         format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
                         datefmt='%m-%d %H:%M',
                         filename=file_name,
                         filemode='w')
-    console = logging.StreamHandler()
+
+    # console日志
+    # 默认sys.stderr
+    console = logging.StreamHandler(sys.stdout)
     console.setLevel(logging.INFO)
     formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
     console.setFormatter(formatter)
     # add the handler to the root logger
-    logging.getLogger('').addHandler(console)
+    # logging.getLogger('').setLevel(logging.DEBUG)
 
+    # ''等同于root日志
+    logging.getLogger('').addHandler(console)
     logger = logging.getLogger('')
     logger.info('info message')
 
+    # 获取root日志
+    logger = logging.getLogger()
+    logger.info('info message')
+    logger.warning("root default level is WARNING")
+
+
+def test_logger():
+    """
+    测试日志类的使用方法。
+    :return:
+    """
+    # 创建一个logger
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    # ***************控制台日志处理器**************
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+
+    # ***************日期循环日志处理器**************
+    log_fmt = '%(asctime)s\tFile \"%(filename)s\",line %(lineno)s\t%(levelname)s: %(message)s'
+    formatter = logging.Formatter(log_fmt)
+    file_path = os.path.join(COMMON_LOG_DIR, "thread")
+    log_file_handler = TimedRotatingFileHandler(filename=file_path, when="S", interval=1, backupCount=7)
+    log_file_handler.suffix = "%Y-%m-%d_%H-%M.log"
+    log_file_handler.extMatch = re.compile(r"^\d{4}-\d{2}-\d{2}_\d{2}-\d{2}.log$")
+    log_file_handler.setFormatter(formatter)
+    log_file_handler.setLevel(logging.DEBUG)
+    logger.addHandler(log_file_handler)
+
+    logger.info("test log")
 
 if __name__ == '__main__':
     pass
-    # test_dictConfig()
-    test_yaml_config()
-    # test_basicConfig()
+    # test_dict_config()
+    # test_yaml_config()
+    # test_basic_config()
+    test_logger()
+
