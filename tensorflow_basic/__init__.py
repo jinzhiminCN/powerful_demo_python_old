dd871bd879b63989fb53f7436c162877555d9323
jinzhimin <jinzhiminCN@163.com>
2018-09-05 11:39:54 +0800

    add model introduction in tensorflow_basic __init__.py

--- tensorflow_basic/__init__.py
+++ tensorflow_basic/__init__.py
@@ -4,4 +4,22 @@
 # tensorflow相关的示例。
 # ==============================================================================
 
-
+# ==============================================================================
+# 传统机器学习模型如下：
+# linear_regression_tf.py
+# nearest_neighbor_tf.py
+# kmeans_tf.py
+# 深度学习模型按照从简单到复杂的顺序排列如下：
+# 深度神经网络基类
+# base_dnn_tf.py
+# 基本神经网络:
+# basic_network.py
+# basic_hidden_network.py
+# 卷积神经网络：
+# convolutional_network.py
+# alexnet_tf.py
+# 循环神经网络：
+# static_rnn_tf.py
+# static_bidirectional_rnn_tf.py
+#
+#
