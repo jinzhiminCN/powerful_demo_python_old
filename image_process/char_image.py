0112dc2c354ffa447a55fc0bedc8b120ccaf2eb5
jinzhimin <jinzhiminCN@163.com>
2018-09-17 08:50:27 +0800

    add char gif

--- image_process/char_image.py
+++ image_process/char_image.py
@@ -4,10 +4,22 @@
 # 字符图像的处理。
 # ==============================================================================
 import os
-from PIL import Image
+from PIL import Image, ImageFont, ImageDraw
+import imageio
+import config.common_config as com_config
+from util.os_util import OsUtil
+from util.log_util import LoggerUtil
+
+# 日志器
+common_logger = LoggerUtil.get_common_logger()
+
+resource_dir = com_config.RESOURCE_DIR
+image_dir = os.path.join(resource_dir, "image_data")
 
 # 定义一个ascii的列表，其实就是让图片上的灰度与字符对应
 ascii_char = list("$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. ")
+# 两帧之间的时间间隔，秒为单位
+DURARION = 0.1
 
 
 def get_char(r, g, b, alpha=256):
@@ -67,10 +79,127 @@ def image_2_char_image(image_path, char_file_path, resize=False):
     char_file.close()
 
 
-if __name__ == "__main__":
-    dir_path = 'E:/images'
+def test_char_image():
+    """
+    测试字符图像。
+    :return:
+    """
+    dir_path = image_dir
     image_name = '145234_164944034631_2.png'
     image_path = os.path.join(dir_path, image_name)
     char_file_path = os.path.join(dir_path, "{0}.txt".format(image_name[:-4]))
     image_2_char_image(image_path, char_file_path, resize=True)
+
+
+def gif2png(file_path):
+    """
+    拆分gif为png。
+    :param file_path:
+    :return:
+    """
+    file_name = os.path.basename(file_path)
+    im = Image.open(file_path)
+
+    dir_path = os.path.join(image_dir, "cache")
+    OsUtil.makedirs(dir_path)
+    os.chdir(dir_path)
+
+    file_path = os.path.join(dir_path, file_name)
+
+    try:
+        while 1:
+            # 获取当前帧数
+            current = im.tell()
+            name = file_path[:-4] + '_' + str(current) + '.png'
+            # gif分割后保存的是索引颜色
+            im.save(name)
+            common_logger.info("png name:{0}".format(name))
+            generate_txt_png(name)
+            im.seek(current + 1)
+    except:
+        os.chdir(dir_path)
+
+
+def generate_txt_png(file_name):
+    """
+    生成txt图片。
+    先将图像转换为txt文本，再将txt转换为图片。
+    :param file_name:
+    :return:
+    """
+    # gif拆分后的图像，需要转换，否则报错，因为gif分割后保存的是索引颜色
+    im = Image.open(file_name).convert('RGB')
+
+    raw_width = im.width
+    raw_height = im.height
+    width = int(raw_width/6)
+    height = int(raw_height/15)
+
+    # 将图像转换为txt文本
+    im = im.resize((width, height), Image.NEAREST)
+    txt = ""
+    colors = []
+    for i in range(height):
+        for j in range(width):
+            pixel = im.getpixel((j, i))
+            colors.append((pixel[0], pixel[1], pixel[2]))
+            if len(pixel) == 4:
+                txt += get_char(pixel[0], pixel[1], pixel[2], pixel[3])
+            else:
+                txt += get_char(pixel[0], pixel[1], pixel[2])
+        txt += '\n'
+        colors.append((255, 255, 255))
+
+    # 将txt转换为图片
+    im_txt = Image.new("RGB", (raw_width, raw_height), (255, 255, 255))
+    dr = ImageDraw.Draw(im_txt)
+    font = ImageFont.load_default().font
+    x = y = 0
+    # 获取字体的宽高
+    font_w, font_h = font.getsize(txt[1])
+    # 调整后更佳
+    font_h *= 1.37
+    # ImageDraw为每个ascii码进行上色
+    for i in range(len(txt)):
+        if txt[i] == '\n':
+            x += font_h
+            y = -font_w
+        dr.text([y, x], txt[i], colors[i])
+        y += font_w
+    name = file_name.split('.')[0]+'_txt'+'.png'
+    common_logger.info("txt png name:{0}".format(name))
+    im_txt.save(name)
+
+
+def png2gif(dir_name):
+    """
+    转换为gif。
+    :param dir_name:
+    :return:
+    """
+    path = os.getcwd()
+    os.chdir(dir_name)
+    dirs = os.listdir()
+    images = []
+    num = 0
+    for image_file in dirs:
+        if image_file.split('_')[-1] == 'txt.png':
+            images.append(imageio.imread(image_file))
+            common_logger.info(image_file)
+            num += 1
+    os.chdir(path)
+
+    gif_name = image_file.split('_')[0]+'_txt_c.gif'
+    common_logger.info(gif_name)
+    imageio.mimsave(gif_name, images, "GIF", duration=DURARION)
+
+
+if __name__ == "__main__":
+    # test_char_image()
+    image_name = 'ce86fd8f5e8e75ab4a914c15ecd1e443.gif'
+    image_path = os.path.join(image_dir, image_name)
+    gif2png(image_path)
+    dir_path = os.path.join(image_dir, "cache")
+    png2gif(dir_path)
+    pass
 
