2c4fa97c88a010418b5a405640dbc21d2bfa1009
jinzhimin <jinzhiminCN@163.com>
2018-09-29 11:08:21 +0800

    add trace in decorate_demo.py

--- python_basic/decorate_demo.py
+++ python_basic/decorate_demo.py
@@ -3,6 +3,10 @@
 # ==============================================================================
 # 装饰器的使用方法。
 # ==============================================================================
+from util.log_util import LoggerUtil
+
+# 日志器
+common_logger = LoggerUtil.get_common_logger()
 
 
 def decorate_without_param(func):
@@ -11,10 +15,10 @@ def decorate_without_param(func):
     :param func:
     :return:
     """
-    print('running decorate', func)
+    common_logger.info('running decorate{0}'.format(func))
 
     def decorate_inner():
-        print('running decorate_inner function')
+        common_logger.info('running decorate_inner function')
         return func()
     return decorate_inner
 
@@ -26,90 +30,127 @@ def decorate_with_before_after(func):
     :return:
     """
     def decorate_inner():
-        print('before decorate_inner function')
+        common_logger.info('before decorate_inner function')
         ret = func()
-        print('after decorate_inner function')
+        common_logger.info('after decorate_inner function')
         return ret
     return decorate_inner
 
 
 def decorate_with_param(func):
+    """
+    包含函数参数的装饰器。
+    :param func:
+    :return:
+    """
     def decorate_inner(*args, **kwargs):
-        print(type(args), type(kwargs))
-        print('args', args, 'kwargs', kwargs)
+        common_logger.info(type(args), type(kwargs))
+        common_logger.info('args', args, 'kwargs', kwargs)
         return func(*args, **kwargs)
     return decorate_inner
 
 
 def decorate_with_name(func):
+    """
+    包含name属性的装饰器。
+    :param func:
+    :return:
+    """
     def decorate_inner(name, *args, **kwargs):
-        print(name, args, kwargs)
+        common_logger.info(name, args, kwargs)
         return func(*args, **kwargs)
     return decorate_inner
 
 
 def decorate_with_default_name(func):
+    """
+    包含默认name属性的装饰器。
+    :param func:
+    :return:
+    """
     def decorate_inner(name="default", *args, **kwargs):
-        print(name, args, kwargs)
-        print(*args)
-        print(*kwargs)
+        common_logger.info(name, args, kwargs)
+        common_logger.info(*args)
+        common_logger.info(*kwargs)
         return func(name, *args, **kwargs)
     return decorate_inner
 
 
 def outer(func):
-    print('enter outer', func)
+    common_logger.info('enter outer {0}'.format(func))
 
     def wrapper():
-        print('running outer')
+        common_logger.info('running outer')
         func()
     return wrapper
 
 
 def inner(func):
-    print('enter inner', func)
+    common_logger.info('enter inner {0}'.format(func))
 
     def wrapper():
-        print('running inner')
+        common_logger.info('running inner')
         func()
     return wrapper
+
+
+def trace(log_level):
+    """
+    装饰器带参数的情况。
+    :param log_level:
+    :return:
+    """
+    def impl_func(func):
+        common_logger.info('{0} Implementing function: "{1}"'.format(log_level, func.__name__))
+        return func
+    return impl_func
 
 
 @outer
 @inner
 def main():
-    print('running main')
+    common_logger.info('running main')
 
 
 @decorate_without_param
 def func_1():
-    print('running func_1')
+    common_logger.info('running func_1')
 
 
 @decorate_with_before_after
 def func_add():
-    print("1 + 2")
+    common_logger.info("1 + 2")
     return 1+2
 
 
 @decorate_with_param
 def func_2(*args, **kwargs):
-    print(args, kwargs)
+    common_logger.info(args, kwargs)
 
 
 @decorate_with_param
 def func_3(name, *args, **kwargs):
-    print(name, args, kwargs)
+    common_logger.info(name, args, kwargs)
 
 
 @decorate_with_name
 def func_4(name, *args, **kwargs):
-    print(name, args, kwargs)
+    common_logger.info(name, args, kwargs)
 
 
 @decorate_with_default_name
 def func_5(name="some_name", *args, **kwargs):
-    print(name, args, kwargs)
+    common_logger.info(name, args, kwargs)
+
+
+@trace('[INFO]')
+def print_msg(msg):
+    common_logger.info(msg)
+
+
+@trace('[DEBUG]')
+def print_assert(expr):
+    common_logger.info(expr)
 
 
 def test_decorate():
@@ -117,22 +158,23 @@ def test_decorate():
     测试装饰器。
     :return:
     """
-    # print("----------执行装饰器函数----------")
+    # common_logger.info("----------执行装饰器函数----------")
     # func_1()
-    # print("----------打印装饰器函数----------")
-    # print(func_1())
-    # print("----------打印装饰器函数名称----------")
-    # print(func_1)
-    # print("----------装饰器函数----------")
+    # common_logger.info("----------打印装饰器函数----------")
+    # common_logger.info(func_1())
+    # common_logger.info("----------打印装饰器函数名称----------")
+    # common_logger.info(func_1)
+    # common_logger.info("----------装饰器函数----------")
     # func_2('x', 'y', para1='1', para2='2')
     # func_3('x', 'y', para1='1', para2='2')
-    func_4('x', 'y', para1='1', para2='2')
+    # func_4('x', 'y', para1='1', para2='2')
     # func_4(nn='x', para1='1', para2='2')
     # func_4(name='x', para1='1', para2='2')
     # func_5(nn='x', para1='1', para2='2')
     # func_5('x2', name2='x', para1='1', para2='2')
     # func_add()
-    # print(func_add())
+    # common_logger.info(func_add())
+    print_msg('Hello, world!')
 
 
 if __name__ == "__main__":
